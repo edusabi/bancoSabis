08978314419a5d466e9f623806880ecd6864e664
EduardoDev <138069501+edusabi@users.noreply.github.com>
2024-07-22 09:08:24 -0300

    Update db.js

--- back/database/db.js
+++ back/database/db.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
 const DbConnect = ()=>{
-    mongoose.connect('mongodb+srv://eduardo:10011997@bancosabis.ob4y4r2.mongodb.net/bancoSabis?retryWrites=true&w=majority&appName=bancoSabis')
-    .then(()=>{
-        console.log("Database rodando!");
-    })
-    .catch((err)=>{
-        console.log("Deu error" + err);
-    });
+    
 
 };
 
